98c3c7219da5c5c553dd0a1bf482027192bab935
MIKURI <and.iks9034@gmail.com>
2022-02-19 17:29:42 +0400

    first commit auth

---
+++ auth.php
@@ -0,0 +1,23 @@
+<? require "header.php" ?>
+
+
+
+<div class="authForm">
+    <form action="auth.php" method="post">
+        <h1 class="authLab">Авторизация</h1>
+        <div class="inputAuthForm">
+            <input type="text" plaseholder="Логин">
+        </div>
+        <div class="inputAuthForm">
+            <input type="password" plaseholder="Пароль">
+        </div>
+        <div class="inputAuthForm">
+            <input type="submit" value="Войти">
+        </div>
+    </form>
+</div>
+
+
+
+
+<? require "footer.php" ?>
