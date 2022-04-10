a20d0b3375fcf7dfaa0c10e66a6428df54883d5e
ABODUS <mr.anonimcool@mail.ru>
2022-04-10 16:24:17 +0400

    &

---
+++ m_sells/add_invoice.php
@@ -0,0 +1,23 @@
+<?session_start();
+    require "../db_connect.php";
+
+    $add_login = $_POST['add_login'];
+    $add_password = sha1(trim($_POST['add_password']));
+    $confirm_pass = sha1(trim($_POST['confirm_pass']));
+    
+
+    if($add_password == $confirm_pass){
+        $autoi = AI($db, "users", "id_user");
+        $result = $db -> prepare("INSERT INTO users VALUES(:id, :add_login, :add_password)");
+        $result -> execute(['id' => $autoi, 'add_login' => $add_login, 'add_password' => $add_password]);
+        header('Location: ../../m_moderation/users.php');
+        exit;
+    }else{
+        $_SESSION['error_pass'] = "Пароли не совпадают!";
+        header('Location: ../../m_moderation/addUser.php');
+        exit;
+    }
+
+    
+
+?>
